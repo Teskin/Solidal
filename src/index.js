973f10726f379a2d8b26e581db60d43ddc275a63
Alberto D'Aleo <alberto.daleo@gmail.com>
2018-05-28 17:45:21 +0200

    Added first responsive grid system to main layout

--- src/index.js
+++ src/index.js
@@ -1,16 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'normalize.css';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import Menu from './component/menu';
-import FullWidthGrid from './component/grid-layout';
+import './App.css';
 
-const App = () => (
-    <MuiThemeProvider>
-        <Menu/>
-        <FullWidthGrid/>
-    </MuiThemeProvider>
-);
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    render() {
+        return (
+            <div className="grid-container" >
+                <header className="grid-item-1">1
+                    <nav>menu</nav>
+                </header>
+                <aside className="grid-item-2">2</aside>
+                <main className="grid-item-3">3</main>
+                <section className="grid-item-4">4</section>
+                <footer className="grid-item-5">5</footer>
+            </div>
+        );
+    }
+}
 
 ReactDOM.render(
     <App/>,
